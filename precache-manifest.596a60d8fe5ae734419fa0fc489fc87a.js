a6eb7d40dc27bb66df9ed05142b680b6d391ecdd
lololToday <xoxwgys56@cs.gwnu.ac.kr>
2020-01-07 15:55:55 +0900

    Updates

---
+++ precache-manifest.596a60d8fe5ae734419fa0fc489fc87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e370831f9ee03a63e508679a99db18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42b1096a892a8359dfe8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7801a016d5047fdbba5",
+    "url": "/static/js/2.b7ffcad0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b7ffcad0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42b1096a892a8359dfe8",
+    "url": "/static/js/main.4483c3d6.chunk.js"
+  },
+  {
+    "revision": "22e5335d5672448025d0",
+    "url": "/static/js/runtime-main.bb6de547.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
