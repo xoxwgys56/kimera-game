673295de272a9d13266c2c651685decd5a3cde17
lololToday <xoxwgys56@cs.gwnu.ac.kr>
2020-01-08 18:59:36 +0900

    Add pages
    Home, NotFound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      kimera game
+      <Router>
+      <Switch>
+        <Route exact path='/' components={Home} />
+        <Route components={NotFound} />
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React, {Fragments } from 'react';
+
+const Home = () => {
+
+    return (
+        <Fragments>
+            Home
+        </Fragments>
+    );
+}
+
+export default Home;
